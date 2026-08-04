5c89c0d1edaa0b4c2308e5edbd31c9d414f463c9
falgunikatekar <falgunikatekar2006.com>
2026-08-04 22:56:18 +0530

    l

---
+++ mini.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+public class mini {
+    public static void main(String[] args) {
+        HashSet <Integer> set=new HashSet<>();
+        set.add(20);
+        set.add(34);
+        set.add(67);
+        set.add(167);
+        System.out.println(set.size());
+    }
+}
---
+++ twopairsum.java
@@ -0,0 +1,11 @@
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        // code here
+        HashSet <Integer> set=new HashSet<>();
+        for(int ele:arr){
+            int rem=target-ele;
+            if(set.contains(rem)) return true;
+            set.add(ele);
+        }return false;
+    }
+}
